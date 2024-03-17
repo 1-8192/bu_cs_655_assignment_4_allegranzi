77f38e630ec50e93fb1f23fbe8313c58ce82a73f
Alessandro Allegranzi <allegranzia@gmail.com>
2024-03-17 19:32:04 -0400

    cleanup

--- src/main/java/edu/bu/met/cs665/CustomerData_HTTPS.java
+++ src/main/java/edu/bu/met/cs665/CustomerData_HTTPS.java
@@ -1,6 +1,30 @@
+/**
+ * Name: ALESSANDRO ALLEGRANZI
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 03/21/2024
+ * File Name: CustomerData_HTTPS
+ * Description: An interface for the new system of retrieving customer data through
+ * a secure HTTPS connection.
+ */
+
 package edu.bu.met.cs665;
 
+/**
+ * An interface for the new system of retrieving customer data through
+ * a secure HTTPS connection.
+ */
 public interface CustomerData_HTTPS {
+  /**
+   * Print customer info based on ID.
+   *
+   * @param customerId the id.
+   */
   void printCustomer(int customerId);
+
+  /**
+   * Load customer based on ID through an HTTPS connection.
+   *
+   * @param customerId the id.
+   */
   void getCustomer_HTTPS(int customerId);
 }
--- src/main/java/edu/bu/met/cs665/CustomerData_USB.java
+++ src/main/java/edu/bu/met/cs665/CustomerData_USB.java
@@ -1,6 +1,31 @@
+/**
+ * Name: ALESSANDRO ALLEGRANZI
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 03/21/2024
+ * File Name: CustomerData_USB
+ * Description: An interface for the new system of retrieving customer data through
+ * a direct USB connection.
+ */
+
 package edu.bu.met.cs665;
 
+/**
+ * An interface for the new system of retrieving customer data through
+ * a direct USB connection.
+ */
 public interface CustomerData_USB {
+
+  /**
+   * Print customer info based on ID.
+   *
+   * @param customerId the id.
+   */
   void printCustomer(int customerId);
+
+  /**
+   * Load customer based on ID through a USB connection.
+   *
+   * @param customerId the id.
+   */
   void getCustomer_USB(int customerId);
 }
